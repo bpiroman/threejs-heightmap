a82adb58839e06f23c98c0286c550ca9bdc52fe7
bpiroman <b.piromanski@holdfasteng.com>
2024-01-03 10:18:36 +0800

    hold point - figuring out the bilinear filtering

--- js/main.js
+++ js/main.js
@@ -100,16 +100,39 @@ window.addEventListener("load", function() {
 	for (var i = 0; i <= vertices.length; i += 3) {
 		let vx = vertices[i];
 		let vy = vertices[i+1];
-		if (vx == -393 && vy == 288.5) {
-			// console.log(data);
-			const position = (vx + dataImg.width * vy) * 4;
-			const dataImageData = dataImg.data;
-			const result = dataImageData[position] / 255.0;
-			console.log(result);
-			vertices[i+2] = result;
-		} else {
-			vertices[i+2] = 0.0;
-		}
+		// console.log(data);
+		const position = (vx + dataImg.width * vy) * 4;
+		const dataImageData = dataImg.data;
+		const result = dataImageData[position] / 255.0;
+		console.log(result);
+		vertices[i+2] = result;
+
+		// Bilinear filter
+		const offset = new THREE.Vector2(-250, -250);
+		const dimensions = new THREE.Vector2(500, 500);
+	
+		// const xf = 1.0 - math.sat((x - offset.x) / dimensions.x);
+		// const yf = math.sat((y - offset.y) / dimensions.y);
+		// const w = this._data.width - 1;
+		// const h = this._data.height - 1;
+	
+		// const x1 = Math.floor(xf * w);
+		// const y1 = Math.floor(yf * h);
+		// const x2 = math.clamp(x1 + 1, 0, w);
+		// const y2 = math.clamp(y1 + 1, 0, h);
+	
+		// const xp = xf * w - x1;
+		// const yp = yf * h - y1;
+	
+		// const p11 = _GetPixelAsFloat(x1, y1);
+		// const p21 = _GetPixelAsFloat(x2, y1);
+		// const p12 = _GetPixelAsFloat(x1, y2);
+		// const p22 = _GetPixelAsFloat(x2, y2);
+	
+		// const px1 = math.lerp(xp, p11, p21);
+		// const px2 = math.lerp(xp, p12, p22);
+	
+		// return math.lerp(yp, px1, px2) * this._params.height;
 	}
 	plane.geometry.attributes.position.needsUpdate = true;
 	plane.geometry.computeVertexNormals();
@@ -118,7 +141,6 @@ window.addEventListener("load", function() {
 // this works
 // window.addEventListener("load", function() { 
 // 	const dataImg = _GetImageData(img1);
-
 // 	const vertices = plane.geometry.attributes.position.array
 // 	for (var i = 0; i <= vertices.length; i += 3) {
 // 		let vx = vertices[i];
